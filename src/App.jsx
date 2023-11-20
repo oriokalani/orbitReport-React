e2387eea16d337330e052d383f2068bcb2502995
oriokalani <orio.kalani@gmail.com>
2023-11-20 15:52:07 -0600

    site not displaying in browser after making edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import React, { useState } from "react";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import satData from "./components/satData";
+
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...newSet(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+ };
+
   return (
-    <div>
+    <>
+      <Banner />
       <Buttons />
-      <Table />
-    </div>
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      <Table sat={sat}/>
+    </>
   );
 }
 
---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+        <div>
+            <header>"Orbit Report"</header>
+            <p>Click on the buttons to see the satellites in the orbit type</p>
+        </div>
+    );  
+};
+
+export default Banner;
