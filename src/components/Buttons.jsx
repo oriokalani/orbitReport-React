1f3de9941415902016176407fc451a7c8241dded
oriokalani <orio.kalani@gmail.com>
2023-11-22 13:56:19 -0600

    updated Buttons, created second button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,19 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import React from "react";
+import satData from "./satData";
+
+//TODO: Should I make this into a named function?
+export const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.Sats.map((sat, id) => 
+        <button onClick = {() => filterByType(sat)} key = {id} >
+          {sat} Orbit
+        </button>
+      )}
+
+      <button onClick = {() => setSat(satData)}
+      >All Orbits</button>
+      
     </div>
   );
 };
-
-export default Buttons;
